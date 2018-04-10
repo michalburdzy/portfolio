b588a85889f9aa9ff08c56df29a7a2441e880d56
mmicalt <mmicalt@gmail.com>
2018-04-10 13:00:12 +0200

    Added contact form

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var allProjects = $('.projects__gallery');
+var projectsSection = $('.projects');
+var contactForm = $('.form');
+
+var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+
+$(window).on('scroll', function() {
+    var positionY = window.pageYOffset;
+    var position_projects = viewportWidth/1.65;  // Header's height = 48vw, so you need to scroll it to 1/1.65 to trigger event
+    var position_contact = viewportWidth/.75;
+
+    if(positionY > position_projects && !projectsSection.hasClass('visible')) {
+            projectsSection.addClass('visible');
+            allProjects.addClass('animation-appear')
+    } else if (positionY > position_contact && !contactForm.hasClass('visible')){
+        contactForm.addClass('animation-appear');
+        console.log('contact!')
+    }
+});
+
