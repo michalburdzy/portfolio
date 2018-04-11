bf9131cedce8190f2cbd9bee12b69aa52e7f25af
mmicalt <mmicalt@gmail.com>
2018-04-11 23:26:01 +0200

    should work now

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 
-// var allProjects = document.querySelectorAll('.projects__gallery');
-// var projectsSection = document.querySelector('.projects');
-// var contactForm = document.querySelector('.form');
-// projectsSection.classList.Add('visible');
+// var allProjects = $('.projects__gallery');
+// var projectsSection = $('.projects');
+// var contactForm = $('.form');
 
-// for (var i = 0; i < allProjects.length; i++) {
-//     allProjects[i].classList.add('animation-appear');
-// }
+window.onload = function(){
+    console.log('window loaded!')
 
+// console.log('js ok')
 
-console.log('js ok!')
+var allProjects = document.querySelectorAll('.projects__gallery');
+var projectsSection = document.querySelector('.projects');
+var contactForm = document.querySelector('.form');
 
 
 var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
@@ -17,14 +18,21 @@ var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerW
 window.addEventListener("scroll", function(){
     var positionY = window.pageYOffset;
     var position_projects = viewportWidth/1.65;  // Header's height = 48vw, so you need to scroll it to 1/1.65 to trigger event
-    var position_contact = viewportWidth/.75;
+    var position_contact = viewportWidth/.65;
 
-    if(positionY > position_projects && !projectsSection.hasClass('visible')) {
-            projectsSection.addClass('visible');
-            allProjects.addClass('animation-appear')
-    } else if (positionY > position_contact && !contactForm.hasClass('visible')){
-        contactForm.addClass('animation-appear');
-        console.log('contact!')
+    if(positionY > position_projects && !projectsSection.classList.contains('visible')) {
+                    // projectsSection.addClass('visible');
+                    // allProjects.addClass('animation-appear')
+            projectsSection.classList.add('visible');
+            for (var i = 0; i < allProjects.length; i++) {
+                allProjects[i].classList.add('animation-appear');
+                console.log('added class to gallery item!')
+            }
+    } else if (positionY > position_contact && !contactForm.classList.contains('visible')){
+        
+    contactForm.classList.add('animation-appear');
+            // contactForm.addClass('animation-appear');
     }
 })
 
+}
